6a324151741fec6bb897301f8644a4ff22f79e51
rotarotarotarota <79619486+rotarotarotarota@users.noreply.github.com>
2021-03-23 18:48:44 +0900

    ユーザー管理機能を実装するにあたって、ビューファイルを適切なものに置き換えた。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
+# 12. Userモデルに各カラムのバリデーションを記述した（「emailとpasswordが空だと保存できない」というバリデーションは標準で用意されているため、記述する必要はない）
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :profile, presence: true
+  validates :occupation, presence: true
+  validates :position, presence: true
+
+  has_many :prototypes
+  has_many :comments
 end
