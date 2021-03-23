3e6dd59f40a3c6cdbb0a48331b2615435ebca18c
rotarotarotarota <79619486+rotarotarotarota@users.noreply.github.com>
2021-03-23 14:21:28 +0900

    アプリケーションが起動することを確認

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,11 @@
+# 6.prototypesコントローラーを作成した
+class PrototypesController < ApplicationController
+  def index
+#    @prototypes = Prototype.includes(:user)
+  end
+end
+# 7.prototypesコントローラーにindexアクションを定義した（アクションの中には何も記述しない
+#def index
+#  @prototypes = Prototype.includes(:user)
+#end
+# 9.エラーが出たため全て記述してみた
